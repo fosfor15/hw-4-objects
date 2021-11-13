2fd5e8394dac8258eafca29e0df0e04421ac837a
Dmitry Kostyuk <dima.kostyuk1508@gmail.com>
2021-11-13 14:18:49 +0300

    Checked

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ car = {
     engine: 'benzin',
     year: 2012
 }
+// Не объявленная сущность car
+// В операциях с присваиванием в конце стоит использовать ;
 
 function showCarInfo(car) {
     console.log('car_1 :>> ', car.mark,
@@ -24,19 +26,25 @@ function showCarInfo(car) {
     'middleSpeed: ' + car.middleSpeed + '\n',
     'type of engine: ' + car.engine + '\n',
     'year of ' + car.year);
+    // Чтобы продемонстрировать, что строки 22-26 на самом деле не являются новыми инструкциями, а являются аргументом у метода console.log, стоит их сдвинуть вправо на 1 отступ
+    // Для каждого будущего потенциального объекта машины будет выводится строка 'car_1' с именем первого объекта машины?
 }
 
 function countTimeOfDraving(distance) {
     var timeDriving = distance / car.middleSpeed;
     var countOfRelax = 0;
+
+    // пропущен пробел между for и ()
     for(var i = 1; i < timeDriving; i++) {
         if (i % 4 == 0 && !(timeDriving % i == 0)) {
             timeDriving ++;
             countOfRelax ++;
+            // Инкременты ++ обычно записываются без пробела перед или после сущнсти, над которой они выполняют свою операцию
         }
     }
     return ('время в пути = ' + timeDriving.toFixed(1) + '\n' +
     'остановок в пути = ' + countOfRelax
+        // Здесь также стоит сделать отступ слева
     );
 }
 
@@ -61,6 +69,8 @@ var denominator;
 
 function createFraction(numerator, denominator) {
     return {numerator:numerator, denominator:denominator};
+    // При использовании внешних сущностей (параметров функции numerator и denominator) как значений для свойств с теми же именами, как у сущностей, можно не дублировать имя сущности в свойсте и присваивать ему значение из сущности, с таким же именем. В итоге у вас могло получиться так: 
+    // return { numerator, denominator };
 }
 
 const fraction1 = createFraction(8, 5);
@@ -68,14 +78,27 @@ const fraction2 = createFraction(9, 13);
 
 const commonDenominator = fraction1.denominator * fraction2.denominator;
 
+
+// Это функция-конструктор
+// Подробнее про них можете почитать здесь https://learn.javascript.ru/constructor-new
 function Operations(fraction1) {
+
+    // Эта функция-конструктор не принимает параметров numerator и denominator – им не откуда взяться, поэтому данная функция как конструктор работать не будет
     this.numerator = numerator;
     this.denominator = denominator;
+
     return fraction1;
+    // У функций-конструкторов не должно быть команды return
 }
+
+
+// Это вывод только что созданных констант с объектами дробей
 console.log(fraction1);
 console.log(fraction2);
+// 
 
+
+// Так же, как вы создаёте метод у функции Operations, вы могли создать простую обычную функцию
 Operations.sum = function(fraction1, fraction2) {
     var result = ((fraction1.numerator * fraction2.denominator) / commonDenominator) + 
     ((fraction2.numerator * fraction1.denominator) / commonDenominator); 
@@ -87,6 +110,8 @@ console.log('Результат сложения = ' + Operations.sum(fraction1,
 Operations.diff = function(fraction1, fraction2) {
     result = ((fraction1.numerator * fraction2.denominator) / commonDenominator) - 
     ((fraction2.numerator * fraction1.denominator) / commonDenominator); 
+    // Сущность result не объявлена
+
     return result.toFixed(3);
 }
 
@@ -103,6 +128,8 @@ console.log('Результат умножения = ' + multipl(fraction1, frac
 function division(fraction1, fraction2) {
     result = ((fraction1.numerator * fraction2.denominator) / 
         (fraction1.denominator * fraction2.numerator));
+    // Сущность result не объявлена
+
     return result.toFixed(3);
 }
 
